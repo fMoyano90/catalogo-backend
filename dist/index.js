3577e5f8e5e3f6a64fe4a162afe90b243a2840b8
Felipe <f.moyano90@gmail.com>
2020-03-06 15:15:59 -0300

    CORS solucionados

--- dist/index.js
+++ dist/index.js
@@ -5,14 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const mongoose_1 = __importDefault(require("mongoose"));
-const cors_1 = __importDefault(require("cors"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const usuario_1 = __importDefault(require("./routes/usuario"));
 const producto_1 = __importDefault(require("./routes/producto"));
 const server = new server_1.default();
-// Configurar CORS
-server.app.use(cors_1.default({ origin: true, credentials: true }));
+// Configurar cabeceras y cors
+server.app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 // Body parser
 server.app.use(body_parser_1.default.urlencoded({ extended: true }));
 server.app.use(body_parser_1.default.json());
--- index.ts
+++ index.ts
@@ -7,8 +7,18 @@ import userRoutes from "./routes/usuario";
 import productoRoutes from "./routes/producto";
 
 const server = new Server();
-// Configurar CORS
-server.app.use(cors({ origin: true, credentials: true }));
+
+// Configurar cabeceras y cors
+server.app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 // Body parser
 server.app.use(bodyParser.urlencoded({ extended: true }));
