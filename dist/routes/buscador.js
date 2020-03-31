e97f085f39e698dadb32728beb89a1981740bc17
Felipe <f.moyano90@gmail.com>
2020-03-31 17:18:25 -0300

    Buscador por usuarios agregado

--- dist/routes/buscador.js
+++ dist/routes/buscador.js
@@ -11,9 +11,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const producto_model_1 = require("../models/producto.model");
+const usuario_model_1 = require("../models/usuario.model");
 const buscadorRoutes = express_1.Router();
 // Obtener producto por busqueda
-buscadorRoutes.get("/:busqueda", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+buscadorRoutes.get("producto/:busqueda", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     let pagina = Number(req.query.pagina) || 1;
     let skip = pagina - 1;
     skip = skip * 10;
@@ -29,6 +30,25 @@ buscadorRoutes.get("/:busqueda", (req, res) => __awaiter(void 0, void 0, void 0,
         ok: true,
         pagina,
         productos
+    });
+}));
+// Obtener usuario por busqueda
+buscadorRoutes.get("usuario/:busqueda", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let pagina = Number(req.query.pagina) || 1;
+    let skip = pagina - 1;
+    skip = skip * 10;
+    let busqueda = req.params.busqueda;
+    var regex = new RegExp(busqueda, "i");
+    const usuarios = yield usuario_model_1.Usuario.find({})
+        .or([{ sap: regex }, { rut: regex }, { nombre: regex }])
+        .sort({ nombre: 1 })
+        .limit(10)
+        .skip(skip)
+        .exec();
+    res.json({
+        ok: true,
+        pagina,
+        usuarios
     });
 }));
 exports.default = buscadorRoutes;
--- routes/buscador.ts
+++ routes/buscador.ts
@@ -1,10 +1,11 @@
 import { Router, Response } from "express";
 import { Producto } from "../models/producto.model";
+import { Usuario } from "../models/usuario.model";
 
 const buscadorRoutes = Router();
 
 // Obtener producto por busqueda
-buscadorRoutes.get("/:busqueda", async (req: any, res: Response) => {
+buscadorRoutes.get("producto/:busqueda", async (req: any, res: Response) => {
   let pagina = Number(req.query.pagina) || 1;
   let skip = pagina - 1;
   skip = skip * 10;
@@ -23,6 +24,29 @@ buscadorRoutes.get("/:busqueda", async (req: any, res: Response) => {
     ok: true,
     pagina,
     productos
+  });
+});
+
+// Obtener usuario por busqueda
+buscadorRoutes.get("usuario/:busqueda", async (req: any, res: Response) => {
+  let pagina = Number(req.query.pagina) || 1;
+  let skip = pagina - 1;
+  skip = skip * 10;
+
+  let busqueda = req.params.busqueda;
+  var regex = new RegExp(busqueda, "i");
+
+  const usuarios = await Usuario.find({})
+    .or([{ sap: regex }, { rut: regex }, { nombre: regex }])
+    .sort({ nombre: 1 })
+    .limit(10)
+    .skip(skip)
+    .exec();
+
+  res.json({
+    ok: true,
+    pagina,
+    usuarios
   });
 });
 
