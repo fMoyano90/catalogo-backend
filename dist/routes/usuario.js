2df5f2234c7df9dbf32ddcd3f5e9501ce1223f19
Felipe <f.moyano90@gmail.com>
2020-03-31 17:42:59 -0300

    Pruebas buscador usuarios

--- dist/routes/usuario.js
+++ dist/routes/usuario.js
@@ -232,21 +232,21 @@ userRoutes.post("/leercsv", (req, res) => {
     });
     // Obtener usuario por busqueda
     userRoutes.get("/busqueda/:busqueda", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-        let pagina = Number(req.query.pagina) || 1;
-        let skip = pagina - 1;
-        skip = skip * 10;
-        let busqueda = req.params.busqueda;
-        var regex = new RegExp(busqueda, "i");
-        const usuarios = yield usuario_model_1.Usuario.find({})
-            .or([{ sap: regex }, { rut: regex }, { nombre: regex }])
-            .sort({ nombre: 1 })
-            .limit(10)
-            .skip(skip)
-            .exec();
+        // let pagina = Number(req.query.pagina) || 1;
+        // let skip = pagina - 1;
+        // skip = skip * 10;
+        // let busqueda = req.params.busqueda;
+        // var regex = new RegExp(busqueda, "i");
+        // const usuarios = await Usuario.find({})
+        //   .or([{ sap: regex }, { rut: regex }, { nombre: regex }])
+        //   .sort({ nombre: 1 })
+        //   .limit(10)
+        //   .skip(skip)
+        //   .exec();
         res.json({
-            ok: true,
-            pagina,
-            usuarios
+            ok: true
+            // pagina,
+            // usuarios
         });
     }));
 });
--- routes/usuario.ts
+++ routes/usuario.ts
@@ -248,24 +248,24 @@ userRoutes.post("/leercsv", (req: any, res: any) => {
 
   // Obtener usuario por busqueda
   userRoutes.get("/busqueda/:busqueda", async (req: any, res: Response) => {
-    let pagina = Number(req.query.pagina) || 1;
-    let skip = pagina - 1;
-    skip = skip * 10;
+    // let pagina = Number(req.query.pagina) || 1;
+    // let skip = pagina - 1;
+    // skip = skip * 10;
 
-    let busqueda = req.params.busqueda;
-    var regex = new RegExp(busqueda, "i");
+    // let busqueda = req.params.busqueda;
+    // var regex = new RegExp(busqueda, "i");
 
-    const usuarios = await Usuario.find({})
-      .or([{ sap: regex }, { rut: regex }, { nombre: regex }])
-      .sort({ nombre: 1 })
-      .limit(10)
-      .skip(skip)
-      .exec();
+    // const usuarios = await Usuario.find({})
+    //   .or([{ sap: regex }, { rut: regex }, { nombre: regex }])
+    //   .sort({ nombre: 1 })
+    //   .limit(10)
+    //   .skip(skip)
+    //   .exec();
 
     res.json({
-      ok: true,
-      pagina,
-      usuarios
+      ok: true
+      // pagina,
+      // usuarios
     });
   });
 });
