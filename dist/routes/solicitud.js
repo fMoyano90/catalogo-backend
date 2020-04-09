a8e6e0be635a6fcb32ec26dddcdc7f8a46001334
Felipe <f.moyano90@gmail.com>
2020-04-09 15:17:25 -0400

    Pruebas nodemailer

--- dist/routes/solicitud.js
+++ dist/routes/solicitud.js
@@ -24,6 +24,7 @@ solicitudRoutes.post('/', [autenticacion_1.verificaToken], (req, res) => __await
     const transporter = nodemailer_1.default.createTransport({
         host: 'smtp.mailtrap.io',
         port: 2525,
+        secure: false,
         auth: {
             user: '5af772fd26f4a4',
             pass: '95176b25d32ae6',
