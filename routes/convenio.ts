d0220abb0d04a3249e900949558627737e4c3d84
Felipe <f.moyano90@gmail.com>
2020-05-18 15:52:22 -0400

    Pruebas convenio

--- routes/convenio.ts
+++ routes/convenio.ts
@@ -13,7 +13,7 @@ convenioRoutes.get("/:genero", async (req: any, res: Response) => {
   const cargo = req.params.cargo;
 
   const eppsConvenio = await Convenio.find({
-    los_andes: 1,
+    los_andes: true,
   });
 
   res.json({
